3ee9a7aa21352d332c2729f1b330056ced997f68
Knitek <rafal96102@gmail.com>
2017-07-14 07:09:58 +0200

    update v0.3.0
    zmniejszenie rozstrzału ue dla sorc, druid, pall; poprawki błędów na
    mapie;

--- spells/scripts/attack/eternal_winter.lua
+++ spells/scripts/attack/eternal_winter.lua
@@ -4,7 +4,7 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_ICETORNADO)
 combat:setArea(createCombatArea(AREA_CROSS5X5))
 
 function onGetFormulaValues(player, level, maglevel)
-	local min = (level / 5) + (maglevel * 5.5) + 25
+	local min = (level / 5) + (maglevel * 7.5) + 25
 	local max = (level / 5) + (maglevel * 11) + 50
 	return -min, -max
 end
--- spells/scripts/attack/hells_core.lua
+++ spells/scripts/attack/hells_core.lua
@@ -4,7 +4,7 @@ combat:setParameter(COMBAT_PARAM_EFFECT, CONST_ME_FIREAREA)
 combat:setArea(createCombatArea(AREA_CIRCLE5X5))
 
 function onGetFormulaValues(player, level, maglevel)
-	local min = (level / 5) + (maglevel * 8) + 50
+	local min = (level / 5) + (maglevel * 9) + 50
 	local max = (level / 5) + (maglevel * 12) + 75
 	return -min, -max
 end
