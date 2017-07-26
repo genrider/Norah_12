5c3f6ccade3a76d09166d61b3a906d795544a104
Knitek <rafal96102@gmail.com>
2017-07-26 10:31:00 +0200

    update v0.4.1
    dodano moba i jego raid: glooth fairy; hatebreeder; the welter;
    osłabiono icicle do bazowej wartości; powiększono kopalnie na
    strongholdzie; zmieniono nagrody warzone;

--- actions/scripts/quests/warzone.lua
+++ actions/scripts/quests/warzone.lua
@@ -1,11 +1,7 @@
 function onUse(cid, item, frompos, item2, topos)
 local config = {
-	[5640] = {item = 18451},
-	[5641] = {item = 18465},
-	[5642] = {item = 18452},
-	[5643] = {item = 18453},
-	[5644] = {item = 18400},
-	[5645] = {item = 18399}
+	[5640] = {item = 23816},
+	[5641] = {item = 2137}
 }
 local p = Player(cid)
 local v = config[item.uid]
